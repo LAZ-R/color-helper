c15f06e0de762842459a3e6ba5eecb48117da70a
LAZ-R <crazydrawer62@gmail.com>
2026-01-29 14:51:54 +0100

    v 0.0.0.2 - Update to 2026 version : using spa-pwa template, added palettes, wcag contrast ratio and css filter

---
+++ app-properties.js
@@ -0,0 +1,15 @@
+export const APP_NAME = "Color Helper";
+export const APP_VERSION = '0.0.0.2';
+export const APP_LOCAL_STORAGE_ID = `color-helper-2026-0001`;
+
+/* 
+# TODO ####################################################
+
+
+
+# CHANGELOG ###############################################
+
+v 0.0.0.2 - Update to 2026 version : using spa-pwa template, added palettes, wcag contrast ratio and css filter
+v 0.0.0.1 - init
+
+*/
